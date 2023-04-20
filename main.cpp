6dad3b900210cc2f4357b5acf936d2f62feed788
PietroProvino <127682352+PietroProvino@users.noreply.github.com>
2023-04-20 08:34:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
+int main(){
+    int n=0;
+    char lista_nomi[3][5] = {"", "", ""};
+    char nome[5] = {""};
+    cin >> lista_nomi[0];
+    cin >> lista_nomi[1];
+    cin >> lista_nomi[2];
+    cout << endl;
+    cin >> nome;
+    for(int i=0; i<3; i++){
+        for(int j=0; j<4; j++){
+            if (lista_nomi[i][j]==nome[j]){
+                n++;
+            }
+        }
+        if( n==4){
+            cout << i;
+        }
 
-int main() {
-   return 0;
+        n= 0;
+
+    }
+    return 0;
 }
